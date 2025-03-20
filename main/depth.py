d08a05ae08e1be8f7d2d57d3a9a4dd325e1b1ec9
mizuki <mizuki.fuji.k@gmail.com>
2025-03-20 15:34:00 +0900

    0320

--- main/depth.py
+++ main/depth.py
@@ -4,6 +4,7 @@ import depthai as dai
 import blobconverter
 import numpy as np
 from pose import frame_norm
+from tools import *
 
 def create_pipeline(stereo):
     pipeline = dai.Pipeline()
@@ -143,4 +144,64 @@ def create_pipeline(stereo):
                 node.io['manip_cfg'].send(cfg)
                 node.io['manip_img'].send(img)
     """)
+    recognition_manip = pipeline.create(dai.node.ImageManip)
+    recognition_manip.initialConfig.setResize(60, 60)
+    recognition_manip.setWaitForConfigInput(True)
+    image_manip_script.outputs['manip_cfg'].link(recognition_manip.inputConfig)
+    image_manip_script.outputs['manip_img'].link(recognition_manip.inputImage)
     
+    # Second stange recognition NN
+    print("Creating recognition Neural Network...")
+    recognition_nn = pipeline.create(dai.node.NeuralNetwork)
+    recognition_nn.setBlobPath(blobconverter.from_zoo(name="head-pose-estimation-adas-0001", shaves=6))
+    recognition_manip.out.link(recognition_nn.input)
+
+    recognition_xout = pipeline.create(dai.node.XLinkOut)
+    recognition_xout.setStreamName("recognition")
+    recognition_nn.out.link(recognition_xout.input)    
+    
+    return pipeline
+
+with dai.Device() as device:
+    stereo = 1 < len(device.getConnectedCameras())
+    device.startPipeline(create_pipeline(stereo))
+
+    sync = TwoStageHostSeqSync()
+    queues = {}
+    # Create output queues
+    for name in ["color", "detection", "recognition"]:
+        queues[name] = device.getOutputQueue(name)
+
+    while True:
+        for name, q in queues.items():
+            # Add all msgs (color frames, object detections and recognitions) to the Sync class.
+            if q.has():
+                sync.add_msg(q.get(), name)
+
+        msgs = sync.get_msgs()
+        if msgs is not None:
+            frame = msgs["color"].getCvFrame()
+            detections = msgs["detection"].detections
+            recognitions = msgs["recognition"]
+
+            for i, detection in enumerate(detections):
+                bbox = frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+
+                # Decoding of recognition results
+                rec = recognitions[i]
+                yaw = rec.getLayerFp16('angle_y_fc')[0]
+                pitch = rec.getLayerFp16('angle_p_fc')[0]
+                roll = rec.getLayerFp16('angle_r_fc')[0]
+                decode_pose(yaw, pitch, roll, bbox, frame)
+
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (10, 245, 10), 2)
+                y = (bbox[1] + bbox[3]) // 2
+                if stereo:
+                    # You could also get detection.spatialCoordinates.x and detection.spatialCoordinates.y coordinates
+                    coords = "Z: {:.2f} m".format(detection.spatialCoordinates.z/1000)
+                    cv2.putText(frame, coords, (bbox[0], y + 60), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 0), 8)
+                    cv2.putText(frame, coords, (bbox[0], y + 60), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255), 2)
+
+            cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
